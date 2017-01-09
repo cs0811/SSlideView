bde3c506b4f50a291a834f8fc518bdf3a57ef3aa
tongxuan <tong_xuan@hunliji.com>
2017-01-09 13:39:52 +0800

    update

--- SSlideView/SSlideView/SSlideView.m
+++ SSlideView/SSlideView/SSlideView.m
@@ -237,11 +237,8 @@ typedef NS_ENUM(NSInteger, SlideViewScrollStatus) {
         return;
     }
     [self.collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForItem:index inSection:0] atScrollPosition:UICollectionViewScrollPositionLeft animated:YES];
-    dispatch_time_t time = dispatch_time(DISPATCH_TIME_NOW, 0.25*NSEC_PER_SEC);
-    dispatch_after(time, dispatch_get_main_queue(), ^{
-        [self scrollViewWillBeginDragging:self.collectionView];
-        self.isScrollFromTabBarView = YES;
-    });
+    [self scrollViewWillBeginDragging:self.collectionView];
+    self.isScrollFromTabBarView = YES;
 }
 
 #pragma mark update
